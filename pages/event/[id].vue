cbab930d8d60400069ade6470bd164b236103918
Maxim Zelenkin <nudepatch@gmail.com>
2022-07-05 16:04:41 +0500

    Scaling Vue with Nuxt.js Lesson 6

--- pages/event/[id].vue
+++ pages/event/[id].vue
@@ -1,14 +1,16 @@
-<template><h1>Event #{{ id }}</h1></template>
+<template><h1>{{ event.title }}</h1></template>
 <script setup>
 const id = useRoute().params.id
 
+const { data: event } = await useFetch('http://localhost:3001/events/' + id)
+
 useHead({
-  title: 'Event #' + id,
+  title: event.title,
   meta: [
     {
       hid: 'description',
       name: 'description',
-      content: 'Where you you need to know about Event #' + id
+      content: 'Where you you need to know about ' + event.title
     }
   ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
-<template><h1>Events</h1></template>
-<script>
-export default {
-  head () {
-    return {
-      title: 'Events Listing'
-    }
-  }
-}
+<template>
+  <div>
+    <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :data-index="index" :event="event"/>
+  </div>
+</template>
+<script setup>
+const { data: events } = await useFetch('http://localhost:3001/events')
+useHead({
+  title: 'Events Listing'
+})
 </script>
