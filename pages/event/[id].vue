c34b04e2cbecf8f222ca34147769c465e9654f9f
Maxim Zelenkin <nudepatch@gmail.com>
2022-07-05 10:44:31 +0500

    Scaling Vue with Nuxt.js Lesson 5

---
+++ pages/event/[id].vue
@@ -0,0 +1,16 @@
+<template><h1>Event #{{ id }}</h1></template>
+<script setup>
+const id = useRoute().params.id
+
+useHead({
+  title: 'Event #' + id,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Where you you need to know about Event #' + id
+    }
+  ]
+})
+
+</script>
